6e2d3af85823893a4a83278553a3dc6dc4d2f6f3
Mike Miklin <MiklinMA@gmail.com>
2025-12-09 15:25:13 +0000

    App

---
+++ App/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var instance = Jellyfin.Plugin.KinopoiskWhite.Api.Instance;
+
+Console.WriteLine(instance);
